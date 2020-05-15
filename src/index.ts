88bc9711f8bc821495d8d074444b361d44968a24
appano1 <thankfather@naver.com>
2020-05-15 14:14:28 +0900

    db 객체 분리

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import express, { Request, Response } from "express";
-import mysql from "mysql";
 import { userRouter } from "./router/user";
 import { fileRouter } from "./router/file";
+import { connection } from "./dbms";
+import mysql from "mysql";
 
-const connection = mysql.createConnection({
-  host: "ec2-3-16-143-85.us-east-2.compute.amazonaws.com",
-  user: "root",
-  password: "1q2w3e!",
-  database: "mustagram",
-});
+const app = express();
+const PORT = 3000;
+
+app.use("/user", userRouter);
+app.use("/file", fileRouter);
+
 connection.connect((err: mysql.MysqlError) => {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -17,16 +18,11 @@ connection.connect((err: mysql.MysqlError) => {
   console.log("Success connecting at mustagram");
 });
 
-const app = express();
-const PORT = 3000;
-
-app.use("/user", userRouter);
-app.use("/file", fileRouter);
-
 app.get("/hello", (req: Request, res: Response) => {
-    res.send("<h1>안녕하세요</h1>");
+  res.send({ data: "Hello World!" });
 });
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
+  // console.log(`Check out ec2-3-16-143-85.us-east-2.compute.amazonaws.com:${PORT}`);
 });
