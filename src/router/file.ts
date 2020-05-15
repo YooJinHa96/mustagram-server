bd012dc40f1ca1f10aac2ae4b74b17ffdbf916c6
appano1 <thankfather@naver.com>
2020-05-15 12:28:03 +0900

    Create new express app

---
+++ src/router/file.ts
@@ -0,0 +1,9 @@
+import { Router, Request, Response } from "express";
+
+const router = Router();
+
+router.get('uploads', (req: Request, res: Response) => {
+    
+});
+
+export const fileRouter = router;
---
+++ src/router/user.ts
@@ -0,0 +1,13 @@
+import { Router, Request, Response } from 'express';
+
+const router = Router();
+
+router.get('/login', (req: Request, res: Response) => {
+
+});
+
+router.get('/sign-in', (req: Request, res: Response) => {
+
+});
+
+export const userRouter = router;
