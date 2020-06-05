fd7fd1b26b2309fd2c7527f6d5f8dc86ebaca313
appano1 <thankfather@naver.com>
2020-06-05 13:47:38 +0900

    Change MySQL server

--- src/dbms.ts
+++ src/dbms.ts
@@ -1,8 +1,8 @@
 import mysql from "mysql";
 
 export const connection = mysql.createConnection({
-  host: "ec2-3-16-143-85.us-east-2.compute.amazonaws.com",
-  user: "root",
+  host: "ec2-18-191-128-120.us-east-2.compute.amazonaws.com",
+  user: "mustadle",
   password: "1q2w3e!",
   database: "mustagram",
 });
