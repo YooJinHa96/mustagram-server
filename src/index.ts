fe0b8695b421384a17c34e401a9fd382ef7d0a1b
appano1 <thankfather@naver.com>
2020-06-05 13:48:35 +0900

    Use body-parser to check body

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,13 @@ import { userRouter } from "./router/user";
 import { fileRouter } from "./router/file";
 import { connection } from "./dbms";
 import mysql from "mysql";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use("/user", userRouter);
 app.use("/file", fileRouter);
