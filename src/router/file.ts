e72146e955a5566c65c36d6b21a53d13a07f3778
appano1 <thankfather@naver.com>
2020-06-05 13:48:12 +0900

    Use Multer for file uploads

--- src/router/file.ts
+++ src/router/file.ts
@@ -1,9 +1,22 @@
 import { Router, Request, Response } from "express";
+import multer from "multer";
 
 const router = Router();
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "./public");
+  },
+  filename: (req, file, callback) => {
+    callback(null, file.originalname);
+  }
+})
 
-router.get('uploads', (req: Request, res: Response) => {
-    
+const uploads = multer({
+  storage
+})
+
+router.get('uploads', uploads.array("files"), (req: Request, res: Response) => {
+  console.log(req.files);
 });
 
 export const fileRouter = router;
