c1da416c5967841e15cb42873a27e2a63ebb17c7
appano1 <thankfather@naver.com>
2020-06-13 14:45:08 +0900

    개인정보 저장과 팔로우 구현

--- src/router/user.ts
+++ src/router/user.ts
@@ -9,7 +9,7 @@ router.post("/login", (req: Request, res: Response) => {
   const { id, password } = req.body;
 
   connection.query(
-    `call log_in('${id}','${password}', @login_sig)`, 
+    `call log_in('${id}','${password}', @login_sig)`,
     (error, results, fields) => {
       if (error) throw error;
       // 1 이상의 값은 유저번호
@@ -17,14 +17,14 @@ router.post("/login", (req: Request, res: Response) => {
       // -2 아이디 오류
       const result = results[0][0].login_sig;
       console.log(result);
-      
+
       res.send(results[0][0].login_sig);
     }
   );
 });
 
 router.post("/sign-in", (req: Request, res: Response) => {
-  const {id, password} = req.body;
+  const { id, password } = req.body;
   connection.query(
     `call join_in('${id}','${password}', @join_sig)`,
     (error, results: ResultType, fields) => {
@@ -34,6 +34,46 @@ router.post("/sign-in", (req: Request, res: Response) => {
       // -1 회원가입 실패
       console.log(results[0][0]);
       res.send(results[0][0].join_sig);
+    }
+  );
+});
+
+router.post("/personal", (req, res) => {
+  const { id, name, sex, intro, birthday } = req.body;
+  console.log(id, name, sex, intro, birthday);
+  connection.query(
+    `call Store_User_Info('${id}', '${name}', '${sex}', '${intro}', '${birthday}', @store_info_sig)`,
+    (error, results, fields) => {
+      if (error) throw error;
+
+      console.log(results[0][0]);
+      res.send(results[0][0].store_info_sig);
+    }
+  );
+});
+
+router.post("/follow", (req, res) => {
+  const { id, friendId } = req.body;
+  connection.query(
+    `call Friend_Follow('${id}', '${friendId}', @follow_sig)`,
+    (error, results, fields) => {
+      if (error) throw error;
+
+      console.log(results[0][0]);
+      res.send(results[0][0].follow_sig);
+    }
+  );
+});
+
+router.post("/unfollow", (req, res) => {
+  const { id, friendId } = req.body;
+  connection.query(
+    `call Friend_Follow('${id}', '${friendId}', @follow_sig)`,
+    (error, results, fields) => {
+      if (error) throw error;
+
+      console.log(results[0][0]);
+      res.send(results[0][0].follow_sig);
     }
   );
 });
